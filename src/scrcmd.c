b4aaa7dd2abed141bab4bd63a685a8fe6c6532d2
kagnus <kagnusdev@gmail.com>
2026-03-08 00:54:02 +0100

    Starter change: Squirtle => Mudkip + more decapitalization

--- src/scrcmd.c
+++ src/scrcmd.c
@@ -1631,8 +1631,8 @@ bool8 ScrCmd_bufferitemname(struct ScriptContext * ctx)
     return FALSE;
 }
 
-static const u8 sText_S[] = _("S");
-static const u8 sText_IES[] = _("IES");
+static const u8 sText_S[] = _("s");
+static const u8 sText_IES[] = _("ies");
 
 bool8 ScrCmd_bufferitemnameplural(struct ScriptContext * ctx)
 {
