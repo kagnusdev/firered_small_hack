0856d56d14d9fe7733562cced8e4b98724c7db29
kagnus <kagnusdev@gmail.com>
2026-03-11 21:02:18 +0100

    Improve blaze kick

--- src/data/battle_moves.h
+++ src/data/battle_moves.h
@@ -3890,9 +3890,9 @@ const struct BattleMove gBattleMoves[MOVES_COUNT] =
     [MOVE_BLAZE_KICK] =
     {
         .effect = EFFECT_BLAZE_KICK,
-        .power = 85,
+        .power = 105,
         .type = TYPE_FIRE,
-        .accuracy = 90,
+        .accuracy = 100,
         .pp = 10,
         .secondaryEffectChance = 10,
         .target = MOVE_TARGET_SELECTED,
